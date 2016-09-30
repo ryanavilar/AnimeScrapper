535365a74fc65efd13896ba0f272080352f1b150
Rifqi Ryan <ryanavilar@Ryans-MP.local>
2016-09-30 13:12:50 +0700

    version 0.0.1

--- index.js
+++ index.js
@@ -4,35 +4,77 @@ var request = require('request-promise');
 var cheerio = require('cheerio');
 var app     = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.set('view engine', 'ejs');
 
-app.get('/scrape', function(req, res){
+app.get('/',function(req,res){
+    res.render('index.ejs');
 
-    var arrayPromise = [];
+})
+
+app.get('/anime/:title', function(req, res){
+
+    var urlMaster = "http://gogoanime.tv/category/";
+    var urlEpisode = "http://gogoanime.tv/";
+    var anime = req.params.title;
+
     var videos = [];
-    var promise = getAnime("http://gogoanime.tv/digimon-adventure-tri-2-ketsui-episode-"+ 1);
-    arrayPromise.push(promise);
-    for(var i = 2; i < 5;i ++){
-        promise = promise.delay(300).then(getAnime("http://gogoanime.tv/digimon-adventure-tri-2-ketsui-episode-"+ i));
-        arrayPromise.push(promise);
-    }
-
-    Promise.all(arrayPromise).then(function(all){
-        //console.log(videos);
-        videos = sortByKeyz(videos,"name");
-        res.render("index.ejs", {videos : videos});
-    })
+    getTotalEpisodes(urlMaster + anime);
+    
     
     var sortByKeyz = function(array, key) {
         return array.sort(function(a, b) {
-            var x = a[key]; var y = b[key];
+            var q = a[key].split("-"); var w = b[key].split("-");
+            var x = parseInt(q[q.length-1]); var y = parseInt(w[w.length-1]);
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         });
     }
 
-    function getAnime(url){
+    function getTotalEpisodes(url){
+        var options = {
+            uri: url,
+            transform: function (body) {
+                return cheerio.load(body);
+            }
+        };
+
+        request(options).then(function($){
+            var episodes = $('#episode_page').children().children().html();
+            var array = episodes.split('-');
+            var start = array[0];
+            var end = array[1];
+            //console.log(end);
+            var arrayPromise = [];
+            if(start == 0) start++;
+            for(var i = start; i <= end;i++){
+                if(anime == "danganronpa-3-the-end-of-kibougamine-gakuen-mirai-hen" && i == 12){
+                    var promise = getAnime(urlEpisode + anime,urlEpisode + anime + "-episode-"+i);
+                }else{
+                    var promise = getAnime(urlEpisode + anime + "-episode-"+i,urlEpisode + anime + "-episode-"+i);
+                }
+                arrayPromise.push(promise);
+            }
+            return Promise.all(arrayPromise).then(function(all){
+
+                videos = sortByKeyz(videos,"name");
+                res.render('anime.ejs',{videos : videos, name : anime});
+            });
+        }).catch(function(err){
+            console.log(err);
+        })
+
+        
+    }
+
+    function getAnime(url,name){
         console.log("proses : " + url);
-        var name = url.split("/");
+        console.log(name);
+        var name = name.split("/");
         var options = {
             uri: url,
             transform: function (body) {
@@ -53,7 +95,28 @@ app.get('/scrape', function(req, res){
     }  
 
 
-})
+});
+
+app.post('/search',function(req,res){
+    var search = req.body.q;
+    var url = "http://gogoanime.tv//search.html?keyword="+search;
+    var options = {
+            uri: url,
+            transform: function (body) {
+                return cheerio.load(body);
+            }
+        };
+    request(options).then(function($){
+        var result = [];
+        var searchResult = $('.anime_movies_items').each(function(i,elem){
+            var splitted = $(this).children().attr('href').split('/');
+            var oneResult = {name : $(this).children().attr('title'), location : splitted[splitted.length-1]}
+            result.push(oneResult);
+        });
+        res.send(result);
+    });
+
+});
 
 app.listen('8081')
 
--- scrape.js
+++ scrape.js
@@ -1,15 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var vlc = require('vlc')([
-  '-I', 'dummy',
-  '-V', 'dummy',
-  '--verbose', '1',
-  '--no-video-title-show',
-  '--no-disable-screensaver',
-  '--no-snapshot-preview',
-  ]);
 
-var url = "http://gogoanime.tv/digimon-adventure-tri-2-ketsui-episode-3";
+
+var url = "http://gogoanime.tv//search.html?keyword=dangan";
 request(url, function(error, response, html){
 
         // First we'll check to make sure no errors occurred when making the request
@@ -18,9 +11,10 @@ request(url, function(error, response, html){
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
             var $ = cheerio.load(html);
-            var newurl = $('.play-video').children().attr('src');
-            console.log(newurl);    
-            
-            
+            //console.log(html);
+            var newurl = $('.anime_movies_items').each(function(i,elem){
+                console.log($(this).children().attr('title'));
+                console.log($(this).children().attr('href'));
+            });
         }
     })
